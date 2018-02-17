06a426659bde0cb381c005f8d8cef5a5e2cc4891
Bjartur Hafþórsson <bjarturhafthors95@gmail.com>
2018-02-17 16:35:50 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Practical1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practical1.dir/main.cpp.o"
+  "Practical1.pdb"
+  "Practical1.exe"
+  "libPractical1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practical1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
