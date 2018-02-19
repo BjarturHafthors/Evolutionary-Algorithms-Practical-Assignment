d56da3f99ae194b7e734121610cba7cda8084ad3
Bart <b.j.vanderlugt@gmail.com>
2018-02-19 12:54:11 +0100

    Added framework for testing

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 void printResults(int fness, int iter, int cx, int ff, int ps);
+bool doTraining(GeneticAlgorithm ga, int cx, int ff, int ps, int is, int ms, int mf);
 
 int main() {
     const int INITIAL_POPULATION_SIZE = 10;
+    const int MAXIMUM_POPULATION_SIZE = 1280;
     const int NUM_TRAINING_ITER = 25;
     const int MIN_TRAINING_SUCC = 24;
 
@@ -14,7 +16,6 @@ int main() {
     int crossoverOperator = ga.uniformX;
     int fitnessFunction = ga.countingOnes;
     int populationSize = INITIAL_POPULATION_SIZE;
-    int succ = 0;
     int maxFitness = 100;
 
     /* TODO: Repeat each experiment 25x for each population size
@@ -22,28 +23,44 @@ int main() {
        TODO: until 24/25 runs return optimal solution*/
 
     // Experiment 1
-    // Rep 25 times
-    for(int n = 0;n<NUM_TRAINING_ITER;n++){
-        bool* bestSolutionFound = ga.run(crossoverOperator, fitnessFunction, populationSize);
-        // Write fitness value
-        printResults(404, n, crossoverOperator,fitnessFunction,populationSize);
-        // Add to amountOfSuccesses
-        if(404 > maxFitness){succ++;}
-    }
+    bool res = doTraining(ga, crossoverOperator, fitnessFunction, populationSize, NUM_TRAINING_ITER, MIN_TRAINING_SUCC, maxFitness);
+
     // If failure --> double popsize and try again.
-    if(succ >= MIN_TRAINING_SUCC){}
-    // If success --> decrease popsize and try again
-    else{}
+    while(!res && populationSize*2 <= MAXIMUM_POPULATION_SIZE){
+        populationSize*=2;
+        res = doTraining(ga, crossoverOperator, fitnessFunction, populationSize,NUM_TRAINING_ITER,MIN_TRAINING_SUCC,maxFitness);
+    }
+    if(populationSize*2 > MAXIMUM_POPULATION_SIZE){std::cout<<std::endl<<"Finished training"<<std::endl;}
+    else{
+        //Perform bisection search to find minimal value of populationSize
+    }
 
     // TODO: Experiments 2 - 5
 
     return 0;
 }
 
+bool doTraining(GeneticAlgorithm ga, int cx, int ff, int ps, int is, int ms, int mf){
+    int count = 0;
+    // Rep 25 times
+    for(int n = 0; n < is; n++){
+        bool* bestSolutionFound = ga.run(cx, ff, ps);
+        //Compute fitness value for bestSolution
+        int fitness = 404;
+
+        // Write fitness value
+        printResults(fitness, n, cx,ff,ps);
+
+        //Add to amount of successes
+        if(fitness == mf){count++;}
+    }
+    return(count >= ms);
+}
+
 void printResults(int fness, int iter, int cx, int ff, int ps){
     std::cout   <<"fitness;"    <<fness
                 <<";iteration;" <<iter
-                <<";cxOP;"      <<cx
+                <<";xOP;"      <<cx
                 <<";fFun;"      <<ff
                 <<";popSize;"   <<ps
                 <<std::endl;
